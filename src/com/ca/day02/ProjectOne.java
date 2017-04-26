b42629cdce621b6c9e5856d64f3f319241f08d05
nuttdguy <pygnasak@gmail.com>
2017-04-25 20:59:58 -0500

    add method to square a number.

--- src/com/ca/day02/ProjectOne.java
+++ src/com/ca/day02/ProjectOne.java
@@ -1,9 +1,23 @@
 package com.ca.day02;
 
+import java.util.Scanner;
+
 public class ProjectOne {
 
+	static Scanner sc;
+	
 	public static void main(String[] args) {	
-		System.out.println("Phouthalang Pygnasak");
+		
+		sc = new Scanner(System.in);
+		System.out.println("Please enter your number: ");
+		
+		int userNum = sc.nextInt();
+		System.out.println("This is the result of the square: " + square(userNum));
+		sc.close();
 	}
 
+	public static int square(int value) {
+		return value * value;
+	}
+	
 }
